c67490ee5af21a444ea7f62b80022ec41bf18824
kwiruu <keiru277@gmail.com>
2026-04-04 02:05:36 +0800

    style: added npm redirect on hero page and nav bar

--- src/routes/site-pages.tsx
+++ src/routes/site-pages.tsx
@@ -431,26 +431,57 @@ export function RootLayout() {
 
             <Separator orientation="vertical" className="my-2" />
 
-            <a
-              className={cn(
-                buttonVariants({ variant: "ghost", size: "sm" }),
-                "h-10 text-muted-foreground gap-2",
-              )}
-              href="https://github.com/kwiruu/taki-cli"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="bi bi-github h-5 w-5 text-black dark:text-white"
-                aria-hidden="true"
+            <div>
+              <a
+                className={cn(
+                  buttonVariants({ variant: "ghost", size: "sm" }),
+                  "h-10 text-muted-foreground gap-2",
+                )}
+                href="https://github.com/kwiruu/taki-cli"
+                target="_blank"
+                rel="noreferrer"
               >
-                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-              </svg>
-              {formatValue(cliData.stats.stars)}
-            </a>
+                <svg
+                  viewBox="0 0 512 512"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  stroke-linejoin="round"
+                  stroke-miterlimit="2"
+                >
+                  <g fill-rule="nonzero">
+                    <path
+                      d="M10.999 500.999v-490h490v490h-490z"
+                      fill="#c12127"
+                    />
+                    <path
+                      d="M102.874 102.874h306.25v306.25h-61.25v-245h-91.875v245H102.874v-306.25z"
+                      fill="#fff"
+                    />
+                  </g>
+                </svg>
+              </a>
+              <a
+                className={cn(
+                  buttonVariants({ variant: "ghost", size: "sm" }),
+                  "h-10 text-muted-foreground gap-2",
+                )}
+                href="https://github.com/kwiruu/taki-cli"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="bi bi-github h-5 w-5 text-black dark:text-white"
+                  aria-hidden="true"
+                >
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                </svg>
+                {formatValue(cliData.stats.stars)}
+              </a>
+            </div>
           </div>
         </div>
       </header>
@@ -479,8 +510,43 @@ export function HomePage() {
           <code className="rounded-md border bg-card px-3 py-2 text-sm">
             npm install -g @kwiruu/taki-cli
           </code>
-          <Link className={buttonVariants()} to="/docs/install">
+          <Link className={buttonVariants({ size: "lg" })} to="/docs/install">
             Quick start
+          </Link>
+          <Link
+            className="w-6 h-6"
+            to="https://www.npmjs.com/package/@kwiruu/taki-cli"
+          >
+            <svg
+              viewBox="0 0 512 512"
+              xmlns="http://www.w3.org/2000/svg"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              stroke-linejoin="round"
+              stroke-miterlimit="2"
+            >
+              <g fill-rule="nonzero">
+                <path d="M10.999 500.999v-490h490v490h-490z" fill="#c12127" />
+                <path
+                  d="M102.874 102.874h306.25v306.25h-61.25v-245h-91.875v245H102.874v-306.25z"
+                  fill="#fff"
+                />
+              </g>
+            </svg>
+          </Link>
+          <Link
+            className="w-6 h-8 flex items-center justify-center"
+            to="https://github.com/kwiruu/taki-cli"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="bi bi-github h-6 w-6 text-black dark:text-white"
+              aria-hidden="true"
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+            </svg>
           </Link>
         </div>
         <p className="text-xs text-muted-foreground">
